c63efd657264149e91b09465e24728bb28344cb3
OmarEiissa <eissa0383@gmail.com>
2024-09-01 22:21:44 +0300

    new project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("scroll", function () {
+  const scrollPosition = window.scrollY;
+  const targetScroll = 170;
+
+  const filesStatsElement = document.querySelector(".files-stats");
+  const filesContentElement = document.querySelector(".files-content");
+
+  if (scrollPosition > targetScroll) {
+    filesStatsElement.classList.add("scroll");
+    filesContentElement.classList.add("scroll");
+  } else {
+    filesStatsElement.classList.remove("scroll");
+    filesContentElement.classList.remove("scroll");
+  }
+});
